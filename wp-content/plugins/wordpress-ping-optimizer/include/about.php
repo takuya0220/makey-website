d08276f645e78cf7c0f8c969aa3bbabd709ddf95
TAKU-PC\TAKU <@japacom.co.jp>
2018-03-09 01:52:02 +0900

    テーマの決定 https://wordpress.org/themes/gk-portfolio/
    プラグインの追加
    https://manablog.org/wordpress-plugin/

---
+++ wp-content/plugins/wordpress-ping-optimizer/include/about.php
@@ -0,0 +1,14 @@
+<style type="text/css">
+	#about{ float: right; width:250px; background: #ffc; border: 1px solid #333; padding: 5px; text-align: justify; }
+	#about p, li, ol{ font-family:verdana; font-size:11px; }
+	#about h3 {text-align:center;}
+	.field_info {text-align:right;};
+</style>
+<?php echo "<div id='about'> 
+	<h3>From the author</h3> 				
+	<p>My name is Pankaj Jha and I've developed WordPress ping Optimizer. Nice to meet you!<p>
+	<h3><a href='https://itspankajha.wordpress.com/portfolio/' target='_blank'>WordPress ping Optimizer  </a><h3>
+	<h3><a href='https://itspankajha.wordpress.com/contact/' target='_blank'>Own the WordPress ping Optimizer plugin </a><h3>
+	<h3> <a href='https://itspankajha.wordpress.com/donation/' target='_blank'>please consider making a small donation</a></h3>
+</div>";
+?>
